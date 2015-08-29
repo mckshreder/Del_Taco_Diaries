b14d0af2429d020b51a5d57e41e503be546cda12
mckshreder <garrettmckenzie@ymail.com>
2015-08-28 22:12:45 -0700

    i am able to access the specific JSON data in my rails controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,19 @@
 class UserController < ApplicationController
+require 'json'
   def index
+    # Sets the client that allows interfacing with Tumblr
+    @client = Tumblr::Client.new({
+      :consumer_key => '76p8HvtPxSMBDbPleLeCBF176HoocXlWIWv0HtEvSWD6qcKwRz',
+      :consumer_secret => 'pysifJfcw3VpIP46cNTQNUC3DsEH9ohNDXaiYhFUDCDUVRHom3',
+      :oauth_token => 'FI6lYUlo9IahuFcQLYBBKkHB9iAJXvMDI3S7X8GBKu8cgEO5NV',
+      :oauth_token_secret => 'ht9sUCr32iFnEAWvIxzIw86r5URJKErITAhGVbKL4mlKOzGWKi'
+    })
+    @posts = @client.posts('mckshreder.tumblr.com')
+    @photos = @posts["posts"]
+    
+    # uri =  HTTParty.get"http://api.tumblr.com/v2/blog/mckshreder/posts?apikey=76p8HvtPxSMBDbPleLeCBF176HoocXlWIWv0HtEvSWD6qcKwRz"
+    # @response = JSON.parse(uri.body)
+
   end
 
   def show
