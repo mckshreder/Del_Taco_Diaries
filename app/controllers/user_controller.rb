b531f214c6d7d546abdc784e8a5d3d4fa9369266
mckshreder <garrettmckenzie@ymail.com>
2015-08-28 22:36:08 -0700

    fully acces one of my photos data

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -8,9 +8,9 @@ require 'json'
       :oauth_token => 'FI6lYUlo9IahuFcQLYBBKkHB9iAJXvMDI3S7X8GBKu8cgEO5NV',
       :oauth_token_secret => 'ht9sUCr32iFnEAWvIxzIw86r5URJKErITAhGVbKL4mlKOzGWKi'
     })
-    @posts = @client.posts('mckshreder.tumblr.com')
-    @photos = @posts["posts"][1]["image_permalink"]
-    
+    @posts = @client.posts('mckshreder.tumblr.com', :type => "photo", :limit => 10)
+    @photos = @posts["posts"][1]["photos"][0]["alt_sizes"][0]["url"]
+  
     # uri =  HTTParty.get"http://api.tumblr.com/v2/blog/mckshreder/posts?apikey=76p8HvtPxSMBDbPleLeCBF176HoocXlWIWv0HtEvSWD6qcKwRz"
     # @response = JSON.parse(uri.body)
 
